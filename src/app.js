bd1fabdb80e6214a698c509b7f01bfd6bcd4f8cc
Karyna Korin <karynakorin@gmail.com>
2022-10-22 14:08:11 -0400

    Added all project files

---
+++ src/app.js
@@ -0,0 +1,96 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+
+  return `${day}, ${hours}:${minutes}`;
+}
+
+function showTemp(response) {
+  document.querySelector("h1").innerHTML = response.data.name;
+  document.querySelector("#main-temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = Math.round(
+    response.data.main.humidity
+  );
+  document.querySelector("#wind-speed").innerHTML = Math.round(
+    response.data.wind.speed * 3.6
+  );
+  document.querySelector("#pressure").innerHTML = response.data.main.pressure;
+  document.querySelector("#feels-like").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+}
+
+function searchCity(city) {
+  let apiKey = "3fdc8cfbf2d6fa0116c9ae92d3df4f79";
+  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemp);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+function retrievePosition(position) {
+  let apiKey = "3fdc8cfbf2d6fa0116c9ae92d3df4f79";
+  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemp);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(retrievePosition);
+}
+
+let cityForm = document.querySelector("#city-form");
+cityForm.addEventListener("submit", handleSubmit);
+let locationButton = document.querySelector("#location-button");
+locationButton.addEventListener("click", getCurrentLocation);
+
+let dateElement = document.getElementById("current-day");
+let currentDate = new Date();
+dateElement.innerHTML = formatDate(currentDate);
+
+searchCity("Montreal");
+
+/*
+function changeCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#main-temperature");
+  temperatureElement.innerHTML = 12;
+}
+
+function changeFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#main-temperature");
+  temperatureElement.innerHTML = 54;
+}
+*/
+
+/* let celciusButton = document.getElementById("celsius-unit");
+celciusButton.addEventListener("click", changeCelsius);
+
+let fahrenheitButton = document.getElementById("fahrenheit-unit");
+fahrenheitButton.addEventListener("click", changeFahrenheit);
+*/
